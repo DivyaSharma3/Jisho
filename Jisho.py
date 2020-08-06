cf10d8488ea3df40e7869002748db12bf164914a
Divya Sharma <divya.sharma.2017@live.rhul.ac.uk>
2020-08-06 23:52:25 +0100

    Commiting script which take a Japanese word, scrapes Jisho for it's definition and outputs markdown for notes.

--- Jisho.py
+++ Jisho.py
@@ -2,201 +2,193 @@ from bs4 import BeautifulSoup
 from requests import get
 from sys import argv
 
+# Kana range in unicode
+START  = ord(u"\u3040") 
+END    = ord(u"\u30FF")
+
+# Unicode of Japanese full stop, kuten.
+KUTEN = u"\u3002"
+
 def handle_definitions(defs):
 
-    """ Returns a list of dictionaries 
-        containing kanji definitions/info """
+  """ Returns a list of dictionaries 
+      containing kanji definitions/info """
 
-    results = []
+  results = []
 
-    # initial_tag = defs.pop(0).text
-    tag = ""
+  # initial_tag = defs.pop(0).text
+  tag = ""
 
-    for d in defs:
+  for d in defs:
 
-        res = ""
+    res = ""
 
-        # if the current thing is a tag, it corresponds to the next definition
-        if d["class"] == ['meaning-tags']:
-            tag = d.text + "\n"
-            continue
+    # if the current thing is a tag, it corresponds to the next definition
+    if d["class"] == ['meaning-tags']:
+      tag = d.text + "\n"
+      continue
 
+    # If the definition gives an example sentence
+    if d.find('div', class_="sentence"):
 
-        # If the definition gives an example sentence
-        if d.find('div', class_="sentence"):
+      example  = d.find_all('span', class_=["furigana", "unlinked"])
+      furigana = [ f.text for f in d.find_all('span', class_="furigana")]
+      sentence = sentence_to_markdown(example, furigana)
 
-            example  = d.find_all('span', class_=["furigana", "unlinked"])
-            furigana = [ f.text for f in d.find_all('span', class_="furigana")]
-            sentence = sentence_to_markdown(example, furigana)
+      ex = [word.text for word in example]
+      res = d.text.replace("\u200b", '\n')
+      res = res.replace(u"\u3002", '\n')
+      res = res.replace(''.join(ex), '\n' + sentence)
 
-            ex = [word.text for word in example]
+      if d.find("span", class_="supplemental_info"):
+        keys = ["definition", "info", "example", "translation"]
 
-            res = d.text.replace("\u200b", '\n')
-            res = res.replace(u"\u3002", '\n')
-            res = res.replace(''.join(ex), '\n' + sentence)
+      else: 
+        keys = ["definition", "example", "translation"]
 
-            # keys = ["definition", "info", "example", "translation"]
+    # If there's no example sentence
+    else:
 
-            if d.find("span", class_="supplemental_info"):
-                keys = ["definition", "info", "example", "translation"]
+      res  = tag + d.text.replace("\u200b", '\n')
+      tag  = ""
+      keys = ["definition", "info"]
 
-            else: 
-                 keys = ["definition", "example", "translation"]
+    values     = res.split('\n')
+    definition = dict(zip(keys, values))
 
-        # In there's no example sentence
-        else:
-            res = tag + d.text.replace("\u200b", '\n')
-            tag = ""
-            # print(res)
-            keys = ["definition", "info"]
-                # if tag:
-                #     res = tag + res
-                #     tag = ""
+    results.append(definition)
 
-        values = res.split('\n')
-
-        # print(values)
-
-        definition = dict(zip(keys, values))
-
-        results.append(definition)
-
-    return results
+  return results
 
 
 
 def get_contained_kanji(contained_info):
 
-    """ Returns a list of dictionaries representing
-        each contained kanji. Keys are: 
-        kanji, definition, kun and on. """
+  """ Returns a list of dictionaries representing
+      each contained kanji. Keys are: 
+      kanji, definition, kun and on. """
 
-    contained = []
+  contained = []
 
-    for k in contained_info:
+  for k in contained_info:
 
-        kanji_info = k.find("div", class_="literal_block")
+    kanji_info  = k.find("div", class_="literal_block")
+    kanji       = (kanji_info.text.strip(), kanji_info.a["href"].replace("//", "https://www."))
+    definitions = k.find('div', class_="meanings english sense").text.replace("\n", "")
 
-        kanji = (kanji_info.text.strip(), kanji_info.a["href"].replace("//", "https://www."))
-        definitions = k.find('div', class_="meanings english sense").text.replace("\n", "")
+    kun_info = k.find("div", class_="kun readings")
+    on_info  = k.find("div", class_="on readings")
 
-        kun_info = k.find("div", class_="kun readings")
-        on_info  = k.find("div", class_="on readings")
+    res = { "kanji": kanji,
+            "definitions": definitions }
 
-        res = { "kanji": kanji,
-                "definitions": definitions }
+    if kun_info:
 
+      k_hrefs    = [a["href"].replace("//", "https://www.") for a in kun_info.find_all("a")]
+      kun        = list(zip(kun_info.text.strip().split()[1:], k_hrefs))
+      res["kun"] = kun
 
-        if kun_info:
+    if on_info:
 
-            k_hrefs = [a["href"].replace("//", "https://www.") for a in kun_info.find_all("a")]
-            kun     = list(zip(kun_info.text.strip().split()[1:], k_hrefs))
+      o_hrefs   = [a["href"].replace("//", "https://www.") for a in on_info.find_all("a")]
+      on        = list(zip(on_info.text.strip().split()[1:], o_hrefs))
+      res["on"] = on
 
-            res["kun"] = kun
+    contained.append(res)
 
-        if on_info:
-            o_hrefs = [a["href"].replace("//", "https://www.") for a in on_info.find_all("a")]
-            on      = list(zip(on_info.text.strip().split()[1:], o_hrefs))
-
-            res["on"] = on
-    
-        contained.append(res)
-
-    return contained
+  return contained
 
 
 def is_kana(char):
-    start, end = ord(u"\u3040"), ord(u"\u30FF")
-    return start <= ord(char) <= end
+  return START <= ord(char) <= END
 
 def get_kanji_furigana(soup):
 
-    furi = soup.find_all("span", class_="kanji")
+  furi = soup.find_all("span", class_="kanji")
 
-    if furi:
+  if furi:
 
-        kanji_text = soup.find("div", class_="concept_light clearfix").find("span", class_="text").text.strip()
-        main_furigana = [ f.text for f in furi ]
+    kanji_text = soup.find("div", class_="concept_light clearfix").find("span", class_="text").text.strip()
+    main_furigana = [ f.text for f in furi ]
 
-        idx = 0
-        for i in range(len(kanji_text)):
-            if not is_kana(kanji_text[i]):
-                idx = i
-                break
+    idx = 0
+    for i in range(len(kanji_text)):
+      if not is_kana(kanji_text[i]):
+        idx = i
+        break
 
-        start = kanji_text[:idx]
+    start = kanji_text[:idx]
 
-        kanji = [kanji_text[idx]]
-        idx += 1
+    kanji = [kanji_text[idx]]
+    idx += 1
 
-        # If the remaning characters are all kana, add them together
-        if kanji_text[idx:] and all(is_kana(char) for char in kanji_text[idx:]):
-            kanji.append(kanji_text[idx:])
+    # If the remaning characters are all kana, add them together
+    if kanji_text[idx:] and all(is_kana(char) for char in kanji_text[idx:]):
+      kanji.append(kanji_text[idx:])
+
+    else:
+      for char in kanji_text[idx:]:
+
+        if is_kana(char):
+          kanji[-1] += char
 
         else:
-            for char in kanji_text[idx:]:
+          kanji.append(char)
+  else:
 
-                if is_kana(char):
-                    kanji[-1] += char
+    kanji = [soup.find("rb").text]
+    main_furigana = [soup.find("rt").text]
 
-                else:
-                    kanji.append(char)
-    else:
-
-        kanji = [soup.find("rb").text]
-        main_furigana = [soup.find("rt").text]
-
-    return start, kanji, main_furigana
+  return start, kanji, main_furigana
 
 def contained_kanji_to_markdown(contained):
 
-    result = (
+  result = (
+    f"## [{contained['kanji'][0]}]({contained['kanji'][1]})\n"
+    f"*{contained['definitions']}*\n"
+  )
 
-        f'## [{contained["kanji"][0]}]({contained["kanji"][1]})\n'
-        f'*{contained["definitions"]}*  \n'
-    )
+  if "kun" in contained:
 
-    if "kun" in contained:
+    result += f"Kun:  "
 
-        result += f'Kun:  '
+    for kun in contained["kun"]:
+      result += f"[{kun[0]}]({kun[1]}), "
 
-        for kun in contained["kun"]:
-            result += f'[{kun[0]}]({kun[1]}), '
+    result = result[:-2] + "\n"
 
-        result = result[:-2] + "  "
+  if "on" in contained:
 
-    if "on" in contained:
+    result += "On   :"
 
-        result += f'\nOn   :'
+    for on in contained["on"]:
+      result += f"[{on[0]}]({on[1]}), "
 
-        for on in contained["on"]:
-            result += f'[{on[0]}]({on[1]}), '
+    result = result[:-2] + "\n"
 
-        result = result[:-2] + "  "
-
-    return result
+  return result
 
 
 def kanji_to_markdown(start, kanji, furigana):
 
-    """ Takes a list of kanji and their corresponding 
-        furigana and returns a string that will display 
-        the kanji with furigana in markdown. """
+  """ Takes a list of kanji and their corresponding 
+      furigana and returns a string that will display 
+      the kanji with furigana in markdown. """
 
-    final = "<ruby> "
+  final = "<ruby> "
 
-    if start:
-        final = start + final
+  if start:
+    final = start + final
 
-    for _ in range(len(furigana)):
-        final += f"{kanji.pop(0)} <rp>(</rp><rt> {furigana.pop(0)} </rt><rp>)</rp> "
-    
-    if kanji:
-        final += f"{''.join(kanji)}"
+  for _ in range(len(furigana)):
+    final += f"{kanji.pop(0)} <rp>(</rp><rt> {furigana.pop(0)} </rt><rp>)</rp> "
+  
+  if kanji:
+    final += f"{''.join(kanji)}"
 
-    final += "</ruby>"
+  final += "</ruby>"
 
-    return final
+  return final
 
 
 def sentence_to_markdown(sentence, furigana):
@@ -222,7 +214,7 @@ def sentence_to_markdown(sentence, furigana):
     else:
       final += word.text
 
-  return final +	u"\u3002"
+  return final +	KUTEN
 
 
 def defs_to_markdown(defs):
@@ -232,20 +224,20 @@ def defs_to_markdown(defs):
       will display the defnition in markdown. """
 
   md = (
-    f'<dt><strong> {defs["definition"]} </strong></dt>\n'
+    f'<dt><strong> {defs["definition"]} </strong></dt><br>\n'
   )
 
   if "info" in defs :
-    md += f'<dd> {defs["info"]} </dd>\n'
+    md += f'  <dd> {defs["info"]} </dd><br>\n'
 
 
   if (len(defs) > 2):
     md += (
-      f'<dd> {defs["example"]} </dd>\n'
-      f'<dd> {defs["translation"]} </dd>\n'
+      f'  <dd> {defs["example"]} </dd>\n'
+      f'  <dd> {defs["translation"]} </dd><br>\n'
     )
 
-  return md + "\n<br>\n"
+  return md
 
 
 def get_kanji(word):
@@ -256,17 +248,17 @@ def get_kanji(word):
   just_kana = True
 
   for i in word:
-      if not is_kana(i):
-          just_kana = False
-          break
+    if not is_kana(i):
+      just_kana = False
+      break
           
   # Get kanji we are defining; turn into markdown to display furigana
   if not just_kana:
-      start, kanji, main_furigana = get_kanji_furigana(soup)
-      kanji_md = kanji_to_markdown(start, kanji, main_furigana)
+    start, kanji, main_furigana = get_kanji_furigana(soup)
+    kanji_md = kanji_to_markdown(start, kanji, main_furigana)
 
   else:
-      kanji_md = kanji_to_markdown(word, [], [])
+    kanji_md = kanji_to_markdown(word, [], [])
 
 
   # Get all definitions
@@ -282,7 +274,7 @@ def get_kanji(word):
   header      = "# " + kanji_md
   definitions = "<dl>\n" + "".join(defs_to_markdown(d) for d in definitions) + "</dl>\n"
   contained   = "## Kanji in this word: \n" if not just_kana else ""
-  link        = f"\n[Open in Jisho...]({url})\n<br>\n"
+  link        = f"\n[Open in Jisho...]({url})\n\n"
   
   for c in contained_kanji:
     contained += contained_kanji_to_markdown(c) + "\n"
--- Notes.md
+++ Notes.md
@@ -1,71 +1,111 @@
-# <ruby> 金 <rp>(</rp><rt> きん </rt><rp>)</rp> 額 <rp>(</rp><rt> がく </rt><rp>)</rp> </ruby>
-<dl>
-<dt><strong> Noun </strong></dt>
-<dd> 1. amount of money </dd>
-
-<br>
-</dl>
-
-## Kanji in this word: 
-## [金](https://www.jisho.org/search/%E9%87%91%20%23kanji)
-*gold*  
-Kun:  [かね、](https://www.jisho.org/search/%E9%87%91%20%E3%81%8B%E3%81%AD), [かな-、](https://www.jisho.org/search/%E9%87%91%20%E3%81%8B%E3%81%AA), [-がね](https://www.jisho.org/search/%E9%87%91%20%E3%81%8C%E3%81%AD)  
-On   :[キン、](https://www.jisho.org/search/%E9%87%91%20%E3%81%8D%E3%82%93), [コン、](https://www.jisho.org/search/%E9%87%91%20%E3%81%93%E3%82%93), [ゴン](https://www.jisho.org/search/%E9%87%91%20%E3%81%94%E3%82%93)  
-## [額](https://www.jisho.org/search/%E9%A1%8D%20%23kanji)
-*forehead, tablet, plaque, framed picture, sum, amount, volume*  
-Kun:  [ひたい](https://www.jisho.org/search/%E9%A1%8D%20%E3%81%B2%E3%81%9F%E3%81%84)  
-On   :[ガク](https://www.jisho.org/search/%E9%A1%8D%20%E3%81%8C%E3%81%8F)  
-
-[Open in Jisho...](https://jisho.org/word/金額)
-<br>
-
 # <ruby> 旅 <rp>(</rp><rt> りょ </rt><rp>)</rp> 行 <rp>(</rp><rt> こう </rt><rp>)</rp> </ruby>
 <dl>
-<dt><strong> Noun, Suru verb </strong></dt>
-<dd> 1. travel; trip; journey; excursion; tour </dd>
-
-<br>
-<dt><strong> Wikipedia definition </strong></dt>
-<dd> 2. Travel </dd>
-
-<br>
+<dt><strong> Noun, Suru verb </strong></dt><br>
+  <dd> 1. travel; trip; journey; excursion; tour </dd><br>
+<dt><strong> Wikipedia definition </strong></dt><br>
+  <dd> 2. Travel </dd><br>
 </dl>
 
 ## Kanji in this word: 
 ## [旅](https://www.jisho.org/search/%E6%97%85%20%23kanji)
-*trip, travel*  
-Kun:  [たび](https://www.jisho.org/search/%E6%97%85%20%E3%81%9F%E3%81%B3)  
-On   :[リョ](https://www.jisho.org/search/%E6%97%85%20%E3%82%8A%E3%82%87)  
+*trip, travel*
+Kun:  [たび](https://www.jisho.org/search/%E6%97%85%20%E3%81%9F%E3%81%B3)
+On   :[リョ](https://www.jisho.org/search/%E6%97%85%20%E3%82%8A%E3%82%87)
+
 ## [行](https://www.jisho.org/search/%E8%A1%8C%20%23kanji)
-*going, journey, carry out, conduct, act, line, row, bank*  
-Kun:  [い.く、](https://www.jisho.org/search/%E8%A1%8C%20%E3%81%84%E3%81%8F), [ゆ.く、](https://www.jisho.org/search/%E8%A1%8C%20%E3%82%86%E3%81%8F), [-ゆ.き、](https://www.jisho.org/search/%E8%A1%8C%20%E3%82%86%E3%81%8D), [-ゆき、](https://www.jisho.org/search/%E8%A1%8C%20%E3%82%86%E3%81%8D), [-い.き、](https://www.jisho.org/search/%E8%A1%8C%20%E3%81%84%E3%81%8D), [-いき、](https://www.jisho.org/search/%E8%A1%8C%20%E3%81%84%E3%81%8D), [おこな.う、](https://www.jisho.org/search/%E8%A1%8C%20%E3%81%8A%E3%81%93%E3%81%AA%E3%81%86), [おこ.なう](https://www.jisho.org/search/%E8%A1%8C%20%E3%81%8A%E3%81%93%E3%81%AA%E3%81%86)  
-On   :[コウ、](https://www.jisho.org/search/%E8%A1%8C%20%E3%81%93%E3%81%86), [ギョウ、](https://www.jisho.org/search/%E8%A1%8C%20%E3%81%8E%E3%82%87%E3%81%86), [アン](https://www.jisho.org/search/%E8%A1%8C%20%E3%81%82%E3%82%93)  
+*going, journey, carry out, conduct, act, line, row, bank*
+Kun:  [い.く、](https://www.jisho.org/search/%E8%A1%8C%20%E3%81%84%E3%81%8F), [ゆ.く、](https://www.jisho.org/search/%E8%A1%8C%20%E3%82%86%E3%81%8F), [-ゆ.き、](https://www.jisho.org/search/%E8%A1%8C%20%E3%82%86%E3%81%8D), [-ゆき、](https://www.jisho.org/search/%E8%A1%8C%20%E3%82%86%E3%81%8D), [-い.き、](https://www.jisho.org/search/%E8%A1%8C%20%E3%81%84%E3%81%8D), [-いき、](https://www.jisho.org/search/%E8%A1%8C%20%E3%81%84%E3%81%8D), [おこな.う、](https://www.jisho.org/search/%E8%A1%8C%20%E3%81%8A%E3%81%93%E3%81%AA%E3%81%86), [おこ.なう](https://www.jisho.org/search/%E8%A1%8C%20%E3%81%8A%E3%81%93%E3%81%AA%E3%81%86)
+On   :[コウ、](https://www.jisho.org/search/%E8%A1%8C%20%E3%81%93%E3%81%86), [ギョウ、](https://www.jisho.org/search/%E8%A1%8C%20%E3%81%8E%E3%82%87%E3%81%86), [アン](https://www.jisho.org/search/%E8%A1%8C%20%E3%81%82%E3%82%93)
+
 
 [Open in Jisho...](https://jisho.org/word/旅行)
-<br>
+
+
+# だらけ<ruby> </ruby>
+<dl>
+<dt><strong> 1. full of (e.g. mistakes); riddled with </strong></dt><br>
+  <dd>  </dd>
+  <dd> この<ruby> 本 <rp>(</rp><rt> ほん </rt><rp>)</rp> </ruby>は<ruby> 間違い <rp>(</rp><rt> まちが </rt><rp>)</rp> </ruby>だらけだ。 </dd><br>
+<dt><strong> 2. covered all over with (blood, mud, etc.) </strong></dt><br>
+  <dd>  </dd>
+  <dd> その<ruby> 部屋 <rp>(</rp><rt> へや </rt><rp>)</rp> </ruby>は紙くずだらけだった。 </dd><br>
+</dl>
+
+
+[Open in Jisho...](https://jisho.org/word/だらけ)
+
 
 # <ruby> 持ち <rp>(</rp><rt> も </rt><rp>)</rp> 帰り <rp>(</rp><rt> かえ </rt><rp>)</rp> </ruby>
 <dl>
-<dt><strong> Noun </strong></dt>
-<dd> 1. takeout (i.e. food); take-out; takeaway; take-away </dd>
-
-<br>
-<dt><strong> Other forms </strong></dt>
-<dd> 持帰り 【もちかえり】 </dd>
-
-<br>
+<dt><strong> Noun </strong></dt><br>
+  <dd> 1. takeout (i.e. food); take-out; takeaway; take-away </dd><br>
+<dt><strong> Other forms </strong></dt><br>
+  <dd> 持帰り 【もちかえり】 </dd><br>
 </dl>
 
 ## Kanji in this word: 
 ## [持](https://www.jisho.org/search/%E6%8C%81%20%23kanji)
-*hold, have*  
-Kun:  [も.つ、](https://www.jisho.org/search/%E6%8C%81%20%E3%82%82%E3%81%A4), [-も.ち、](https://www.jisho.org/search/%E6%8C%81%20%E3%82%82%E3%81%A1), [も.てる](https://www.jisho.org/search/%E6%8C%81%20%E3%82%82%E3%81%A6%E3%82%8B)  
-On   :[ジ](https://www.jisho.org/search/%E6%8C%81%20%E3%81%98)  
+*hold, have*
+Kun:  [も.つ、](https://www.jisho.org/search/%E6%8C%81%20%E3%82%82%E3%81%A4), [-も.ち、](https://www.jisho.org/search/%E6%8C%81%20%E3%82%82%E3%81%A1), [も.てる](https://www.jisho.org/search/%E6%8C%81%20%E3%82%82%E3%81%A6%E3%82%8B)
+On   :[ジ](https://www.jisho.org/search/%E6%8C%81%20%E3%81%98)
+
 ## [帰](https://www.jisho.org/search/%E5%B8%B0%20%23kanji)
-*homecoming, arrive at, lead to, result in*  
-Kun:  [かえ.る、](https://www.jisho.org/search/%E5%B8%B0%20%E3%81%8B%E3%81%88%E3%82%8B), [かえ.す、](https://www.jisho.org/search/%E5%B8%B0%20%E3%81%8B%E3%81%88%E3%81%99), [おく.る、](https://www.jisho.org/search/%E5%B8%B0%20%E3%81%8A%E3%81%8F%E3%82%8B), [とつ.ぐ](https://www.jisho.org/search/%E5%B8%B0%20%E3%81%A8%E3%81%A4%E3%81%90)  
-On   :[キ](https://www.jisho.org/search/%E5%B8%B0%20%E3%81%8D)  
+*homecoming, arrive at, lead to, result in*
+Kun:  [かえ.る、](https://www.jisho.org/search/%E5%B8%B0%20%E3%81%8B%E3%81%88%E3%82%8B), [かえ.す、](https://www.jisho.org/search/%E5%B8%B0%20%E3%81%8B%E3%81%88%E3%81%99), [おく.る、](https://www.jisho.org/search/%E5%B8%B0%20%E3%81%8A%E3%81%8F%E3%82%8B), [とつ.ぐ](https://www.jisho.org/search/%E5%B8%B0%20%E3%81%A8%E3%81%A4%E3%81%90)
+On   :[キ](https://www.jisho.org/search/%E5%B8%B0%20%E3%81%8D)
+
 
 [Open in Jisho...](https://jisho.org/word/持ち帰り)
-<br>
+
+
+# <ruby> 仕 <rp>(</rp><rt> し </rt><rp>)</rp> 事 <rp>(</rp><rt> ごと </rt><rp>)</rp> </ruby>
+<dl>
+<dt><strong> 1. work; job; labor; labour; business; task; assignment; occupation; employment </strong></dt><br>
+<dt><strong> Noun </strong></dt><br>
+  <dd> 2. work </dd><br>
+<dt><strong> Wikipedia definition </strong></dt><br>
+  <dd> 3. Work </dd><br>
+</dl>
+
+## Kanji in this word: 
+## [仕](https://www.jisho.org/search/%E4%BB%95%20%23kanji)
+*attend, doing, official, serve*
+Kun:  [つか.える](https://www.jisho.org/search/%E4%BB%95%20%E3%81%A4%E3%81%8B%E3%81%88%E3%82%8B)
+On   :[シ、](https://www.jisho.org/search/%E4%BB%95%20%E3%81%97), [ジ](https://www.jisho.org/search/%E4%BB%95%20%E3%81%98)
+
+## [事](https://www.jisho.org/search/%E4%BA%8B%20%23kanji)
+*matter, thing, fact, business, reason, possibly*
+Kun:  [こと、](https://www.jisho.org/search/%E4%BA%8B%20%E3%81%93%E3%81%A8), [つか.う、](https://www.jisho.org/search/%E4%BA%8B%20%E3%81%A4%E3%81%8B%E3%81%86), [つか.える](https://www.jisho.org/search/%E4%BA%8B%20%E3%81%A4%E3%81%8B%E3%81%88%E3%82%8B)
+On   :[ジ、](https://www.jisho.org/search/%E4%BA%8B%20%E3%81%98), [ズ](https://www.jisho.org/search/%E4%BA%8B%20%E3%81%9A)
+
+
+[Open in Jisho...](https://jisho.org/word/仕事)
+
+
+# <ruby> 部 <rp>(</rp><rt> へや </rt><rp>)</rp> 屋</ruby>
+<dl>
+<dt><strong> 1. room; chamber </strong></dt><br>
+  <dd> マユコ </dd>
+  <dd> は<ruby> 部屋 <rp>(</rp><rt> へや </rt><rp>)</rp> </ruby>に<ruby> 入った <rp>(</rp><rt> はい </rt><rp>)</rp> </ruby>。 </dd><br>
+<dt><strong> 2. apartment; flat; pad </strong></dt><br>
+  <dd> わたし私のへや部屋は４かい階にあります </dd>
+  <dd> My apartment is on the fourth floor. </dd><br>
+<dt><strong> Noun </strong></dt><br>
+  <dd> 3. stable </dd><br>
+<dt><strong> Wikipedia definition </strong></dt><br>
+  <dd> 4. Room </dd><br>
+</dl>
+
+## Kanji in this word: 
+## [部](https://www.jisho.org/search/%E9%83%A8%20%23kanji)
+*section, bureau, dept, class, copy, part, portion, counter for copies of a newspaper or magazine*
+Kun:  [-べ](https://www.jisho.org/search/%E9%83%A8%20%E3%81%B9)
+On   :[ブ](https://www.jisho.org/search/%E9%83%A8%20%E3%81%B6)
+
+## [屋](https://www.jisho.org/search/%E5%B1%8B%20%23kanji)
+*roof, house, shop, dealer, seller*
+Kun:  [や](https://www.jisho.org/search/%E5%B1%8B%20%E3%82%84)
+On   :[オク](https://www.jisho.org/search/%E5%B1%8B%20%E3%81%8A%E3%81%8F)
+
+[Open in Jisho...](https://jisho.org/word/部屋)
 
